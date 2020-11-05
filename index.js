7ef5e9557e0bc60b08ae9a78e5e7ba0a7c41f8d2
Tammy Lam <lamtammy1515@gmail.com>
2020-11-05 10:20:45 -0600

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-const driver = {};
+const driver = {}
 
-function updateDriverWithKeyAndValue(driver, key, value) {
-  // Alternate using ES6 Spread operators:
-  // return { ...driver, ...{ [key]: value } }
-  return Object.assign({}, driver, { [key]: value });
+function updateDriverWithKeyAndValue(driver, key, value){
+    return Object.assign({}, driver, { [key]: value });
+  
 }
 
-function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-  driver[key] = value;
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key]=value;
 
   return driver;
 }
 
-function deleteFromDriverByKey(driver, key) {
-  // Alternate using ES6 Spread operators:
-  // const newObj = { ...driver }
+function deleteFromDriverByKey(driver, key){
   const newObj = Object.assign({}, driver);
 
   delete newObj[key];
 
   return newObj;
 }
-
 function destructivelyDeleteFromDriverByKey(driver, key) {
   delete driver[key];
 
